ae83404883a9de4650ca0afeaeb64bf8f0ca2d07
namramoradiya <moradiyanamra@gmail.com>
2026-01-12 18:25:13 +0530

    Pushing into the Email

--- app/crud/student.py
+++ app/crud/student.py
@@ -1,8 +1,13 @@
 from sqlalchemy.orm import Session
 from app.models.student import Student
 from app.schemas.student import StudentCreate
+from fastapi import HTTPException
 
 def create_student(db: Session, student: StudentCreate):
+    if get_student_by_email(db, student.email):
+        raise HTTPException(status_code=400,detail="Email already registered")
+    
+    
     db_student = Student(
         name=student.name,
         email=student.email,
@@ -12,3 +17,6 @@ def create_student(db: Session, student: StudentCreate):
     db.commit()
     db.refresh(db_student)
     return db_student
+
+def get_student_by_email(db: Session, email: str):
+    return db.query(Student).filter(Student.email == email).first()
